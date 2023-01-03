833d7b4f1b26e0bdadd317fc8dfc0b7f22048ee2
Abu Adan <abuadan@MacBook-Pro.local>
2023-01-03 15:26:29 +0000

    working state of nvim, exlcluding dap

---
+++ lua/config/coding/neorg.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+function M.setup()
+  require("neorg").setup {
+    load = {
+      ["core.defaults"] = {},
+      ["core.norg.concealer"] = {},
+      ["core.norg.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },
+      ["core.norg.dirman"] = {
+        config = {
+          workspaces = {
+            my_workspace = "~/$DEV_HOME/notes",
+          },
+        },
+      },
+    },
+  }
+end
+
+return M
