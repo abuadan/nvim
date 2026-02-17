f5dc512fe16e463f7d8cb48b92a8bbaaa3ef23e0
Abu Adan <abuadan@gmail.com>
2026-02-17 17:36:59 +0000

    Remove modicator and neotree, add picker and snacks

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,46 +1,4 @@
-local Util = require("lazyvim.util")
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  requires = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  opts = {
-    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-      hide_by_name = {
-        "node_modules",
-      },
-      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-        ".DS_Store",
-        "thumbs.db",
-      },
-    },
-    window = {
-      mappings = {
-        ["<space>"] = "none",
-        ["Y"] = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          vim.fn.setreg("+", path, "c")
-        end,
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-    },
-  },
+  enabled = false,
 }
